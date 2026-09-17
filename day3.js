5bcf5da47c5b77c42e37b01c149f40fd356b1e33
Ира <sokolenkoira243@gmail.com>
2026-09-17 22:43:33 +0500

    День 3: Условия if/else

---
+++ day3.js
@@ -0,0 +1,27 @@
+let age=17;
+
+if (age >= 18){
+    console.log("Совершеннолетняя");
+} else {
+    console.log("Ещё рано");
+}
+
+let score=3;
+
+if (score===5) {
+    console.log("Отлично");
+} else if (score===4){
+    console.log("Хорошо");
+} else if (score===3){
+    console.log("Удволетворительно");
+}else {
+    console.log("Надо учть JS");
+}
+
+let number =8;
+
+if (number %2===0){
+    console.log(number+"-четное");
+} else{
+    console.log(number+"-нечетное");
+}
