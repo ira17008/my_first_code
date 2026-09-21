5082cf26662310658808f0f55ef496d1ca7ff38f
Ира <sokolenkoira243@gmail.com>
2026-09-21 21:59:17 +0500

    День 7: калькулятор в консоли — финальная версия

--- calculator.js
+++ calculator.js
@@ -1,4 +1,20 @@
+function isValidOperator(operator){
+    return operator === "+" ||
+           operator === "-" ||
+           operator === "*" ||
+           operator === "/" ||
+           operator === "%" ||
+           operator === "**";
+}
+
+console.log(isValidOperator("+"));    
+console.log(isValidOperator("abc"));  
+
+
 function calculator(a, b, operator) {
+    if (!isValidOperator(operator)){
+        return "Ошибка: Такого оператора не существует";
+    }
     switch (operator) {
         case "+":
             return a + b;
@@ -6,6 +22,10 @@ function calculator(a, b, operator) {
             return a - b;
         case "*":
             return a * b;
+        case "**":
+            return a ** b;
+        case "%":
+            return a % b;
         case "/":
             if (b === 0) {
                 return "Ошибка: Нельзя поделить на 0";
@@ -13,7 +33,7 @@ function calculator(a, b, operator) {
                 return a / b;
             }
         default:
-            return "Ошибка: Такого оператора не существует";
+            return "Ошибка: Неизвестная ошибка";
     }
 }
 
@@ -25,3 +45,6 @@ console.log(calculator(2, 5, "*"));
 console.log(calculator(10, 2, "/"));  
 console.log(calculator(5, 0, "/"));  
 console.log(calculator(5, 2, "abc")); 
+console.log(calculator(2, 3, "**"));
+console.log(calculator(10, 3, "%")); 
+
